c5d0114c2c79898df22913aac5b1c8569cd5efb8
Lucas Sato <lucasjks@gmail.com>
2022-10-11 21:06:17 -0500

    Add GPU support for GPT2Engine
    Also, add batching to GPT2Engine.predict_document

--- src/gpt2.py
+++ src/gpt2.py
@@ -7,17 +7,21 @@ GPT2_MAX_SEQ_LEN = 1024
 class GPT2Engine(object):
 
     def __init__(self) -> None:
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
         self.tokenizer.pad_token = self.tokenizer.eos_token
         self.model = GPT2LMHeadModel.from_pretrained(
                 'gpt2',
                 pad_token_id=self.tokenizer.eos_token_id,
                 temperature=0)
-        self.model.eval()
+        if self.device == 'cuda':
+            self.model.eval().cuda()
+        else:
+            self.model.eval()
 
 
     def tokenize(self, text: str) -> torch.Tensor:
-        return self.tokenizer.encode(text, return_tensors='pt')
+        return self.tokenizer.encode(text, return_tensors='pt').to(self.device)
 
 
     def generate_text(self, prompt: str, num_tokens: int=5, num_candidates: int=1) -> list:
@@ -31,7 +35,7 @@ class GPT2Engine(object):
         return self.tokenizer.decode(output_token_ids[0], clean_up_tokenization_spaces=True)
 
 
-    def predict_document(self, document: str, window_size: int=25) -> tuple:
+    def predict_document(self, document: str, window_size: int=25, batch_size: int=100) -> tuple:
         '''
         Slide a window of window_size tokens across an entire document. At each
         step, use the window contents as a prompt, and generate a prediction
@@ -49,7 +53,15 @@ class GPT2Engine(object):
         if input_token_ids.shape[1] < window_size - 1:
             raise ValueError('Window contains entire document.')
         prompts = input_token_ids.squeeze(0).unfold(0, window_size, 1)[:-1]
-        predictions = self.model.generate(input_ids=prompts,
-                                          max_length=window_size+1,
-                                          do_sample=False)
-        return predictions, input_token_ids[:, window_size:].view(-1, 1)
+        batch_beg = 0
+        batch_end = batch_size
+        predictions = []
+        while batch_end < prompts.shape[0]:
+            batch = prompts[batch_beg:batch_end]
+            predictions.append(self.model.generate(input_ids=batch,
+                                                   max_length=window_size+1,
+                                                   do_sample=False))
+            batch_beg += batch_size
+            batch_end += batch_size
+        return (torch.cat(predictions, dim=0),
+                input_token_ids[:, window_size:].view(-1, 1))
